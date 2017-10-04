2ac40a5da3c258e8bb1b06f2daaedd7858c08113
Jonathan Pelham <jonititan@gmail.com>
2017-10-04 10:17:32 +0100

    Create CommandLineStart.py

---
+++ CommandLineStart.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+'''
+@author: jonititan
+'''
+
+import subprocess
+# --fdm=null turns the fdm system off.
+cl_options = {
+    'telnet':5500,
+    'httpd':8080,
+    'aircraft':'A330-223',
+    'airport':'KBOS',
+    'heading':0,
+    'timeofday':'noon',
+    'altitude':36000,
+    'vc':270,
+    'glideslope':1,
+    'roc':0,
+    'roll':0,
+}
+properties = {
+    '/autopilot/settings/target-altitude-ft':36000,
+    '/autopilot/settings/target-speed-kt':270,
+    '/autopilot/locks/altitude':'altitude-hold',
+    '/autopilot/locks/speed':'speed-with-throttle',
+    '/autopilot/locks/heading':'wing-leveler',
+    '/engines/engine[0]/running':'true',
+    '/engines/engine[1]/running':'true'
+    
+    
+        }
+parsed_props = ['--prop:'+key_val+'='+str(properties[key_val]) for key_val in properties.keys()]
+parsed_options = ['--'+key_val+'='+str(cl_options[key_val]) for key_val in cl_options.keys() if cl_options[key_val] != False]
+parsed_options.extend(parsed_props)
+subprocess.Popen(r'/usr/games/fgfs '+' '.join(parsed_options), shell=True)
